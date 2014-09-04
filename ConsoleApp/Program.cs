4bf0f133e4d6dec15d22cd2be2ba43f036c52463
tsteck <tyler.steck@landesk.com>
2014-09-04 08:07:36 -0600

    Doing some more work on app part 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp
             PrinterHelper.PrinterHelperMethod("We love to party!");
 
             PrinterHelper.PrinterHelperMethod("Doing some more work here... " + (1 + 2));
-
+            PrinterHelper.PrinterHelperMethod("Looks like we have some math: " + Math.Sqrt(36));
 
         }
     }
